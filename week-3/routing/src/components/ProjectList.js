b8bd05df320ec319c5ec137e4180cd9b2937f223
Marcos <mp2772@gmail.com>
2020-10-07 18:33:46 -0400

    added week 3 folder and routing example

---
+++ week-3/routing/src/components/ProjectList.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const myProjects = [
+    {
+        id: "1a",
+        name: "The Frogger Clone",
+        year: 2017,
+        technologies: "JavaScript, jQuery",
+        description: "The first project game clone.",
+    },
+    {
+        id: "2b",
+        name: "iTravel",
+        year: 2017,
+        technologies: "Mongo DB, ExpressJS, NodeJS, JavaScript, HTML, CSS",
+        description:
+            "Web App that allows logged in users to share their experiences about travel destinations.",
+    },
+    {
+        id: "3c",
+        name: "The Plan",
+        year: 2017,
+        technologies:
+            "Mongo DB, ExpressJS, Angular2, NodeJS, JavaScript, HTML, CSS",
+        description:
+            "Web App that allows logged in users to plan your next activity with your friends or business partners.",
+    },
+];
+
+export const Projects = (props) => {
+    console.log({ props });
+    return (
+        // <div>
+        //     <h2>Projects:</h2>
+        //     {myProjects.map((eachProject, index) => {
+        //         return (
+        //             <div key={eachProject.id}>
+        //                 <h3>{eachProject.name}</h3>
+        //                 <h4>{eachProject.technologies}</h4>
+        //                 <hr />
+        //             </div>
+        //         );
+        //     })}
+        // </div>
+
+        // the above code is the same as below except that you can now route to the details page for the project details.
+
+        <div>
+            <h2>Projects:</h2>
+            <hr />
+            <button onClick={props.changeRedirect}>
+                Logged Out? : {String(props.redirect)}
+            </button>
+            <hr />
+            {myProjects.map((eachProject, index) => {
+                return (
+                    <div key={eachProject.id}>
+                        <h3>
+                            <Link to={`/projects/${eachProject.id}`}>
+                                {eachProject.name}
+                            </Link>
+                        </h3>
+                        <h4>{eachProject.technologies}</h4>
+                        <hr />
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
