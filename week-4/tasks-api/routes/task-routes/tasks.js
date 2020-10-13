b47a8d86b1e0999ce2fe5ca31e02ed501dda8482
Marcos <mp2772@gmail.com>
2020-10-12 21:02:19 -0400

    changed create route to top of task route file

--- week-4/tasks-api/routes/task-routes/tasks.js
+++ week-4/tasks-api/routes/task-routes/tasks.js
@@ -2,16 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Task = require("../../models/Task.js");
 
-router.get("/all-tasks", (req, res, next) => {
-    Task.find()
-        .then((allTasks) => {
-            res.status(200).json(allTasks);
-        })
-        .catch((err) =>
-            res.status(500).json({ message: "Error finding all tasks" })
-        );
-});
-
 router.post("/create", (req, res, next) => {
     console.log({ body: req.body });
     Task.create(req.body)
@@ -20,6 +10,16 @@ router.post("/create", (req, res, next) => {
         })
         .catch((err) =>
             res.status(400).json({ message: "Error while creating task" })
+        );
+});
+
+router.get("/all-tasks", (req, res, next) => {
+    Task.find()
+        .then((allTasks) => {
+            res.status(200).json(allTasks);
+        })
+        .catch((err) =>
+            res.status(500).json({ message: "Error finding all tasks" })
         );
 });
 
